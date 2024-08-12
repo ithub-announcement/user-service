648b646b7aee4a069f88a30277150d4cb482fd37
Дмитрий Чехонадских <loseex@vk.com>
2024-08-12 12:20:09 +0500

    Написал JWT Provider
    - Написал провайдер для работы с JWT токенами.
    - В провайдере написал метод генерации токена по username и validity.
    - В провайдере написал метод валидации токена.
    - В провайдере написал метод чтения токена.
    - В провайдере написал метод декодирования токена в SecretKey.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,10 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+  // https://mvnrepository.com/artifact/io.jsonwebtoken/jjwt
+  implementation("io.jsonwebtoken:jjwt:0.9.1")
+
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ src/main/kotlin/ru/itcollege/userservice/core/providers/JwtProvider.kt
@@ -0,0 +1,100 @@
+package ru.itcollege.userservice.core.providers
+
+import io.jsonwebtoken.*
+import org.springframework.stereotype.Component
+import java.util.*
+import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
+
+/**
+ * # JwtProvider
+ *
+ * Провайдер для работы с JWT токенами.
+ *
+ * @author Дмитрий Чехонадских <loseex@vk.com>
+ * */
+
+@Component
+class JwtProvider {
+
+  /**
+   * jwtAccessSecret
+   *
+   * Секретный ключ для JWT токенов.
+   *
+   * todo: Нужно перенести значение в конфиг.
+   * */
+
+  private final var jwtAccessSecret = "bXkgc3VwZXIgZ3JvdXAgc2VjcmV0LWtleS1rZXkgc3RyaW5n"
+
+  /**
+   * ## decode
+   *
+   * Перевод строки secret в SecretKey.
+   *
+   * @param secret
+   * */
+
+  private fun decode(secret: String): SecretKey {
+    val decodeKey = Base64.getDecoder().decode(secret)
+    return SecretKeySpec(decodeKey, 0, decodeKey.size, "HmacSHA512")
+  }
+
+  /**
+   * ## secret (var)
+   *
+   * Переменная с секретным ключом для построения JWT.
+   * Объявляется в application.properties, но перед этим переведена в SecretKey.
+   *
+   * */
+
+  private var secret: SecretKey = this.decode(this.jwtAccessSecret)
+
+  /**
+   * ## generate
+   *
+   * Генерация JWT токена.
+   *
+   * @param username
+   * @param validity (default is ${jwt.access-token-validity-hours})
+   * */
+
+  fun generate(username: String, validity: Long): String {
+    val now: Date = Date()
+    val expired: Date = Date(now.time + validity * 60 * 60 * 1000)
+    return Jwts.builder().setIssuedAt(now).setExpiration(expired).setSubject(username)
+      .signWith(SignatureAlgorithm.HS512, this.secret).compact()
+  }
+
+  /**
+   * ## isValidate
+   *
+   * Валидация JWT токена.
+   *
+   * @param token
+   * */
+
+  fun isValidate(token: String): Boolean {
+    try {
+      Jwts.parser().setSigningKey(this.secret).parseClaimsJwt(token)
+      return true
+    } catch (error: ExpiredJwtException) {
+      return false
+    } catch (error: SignatureException) {
+      return false
+    }
+  }
+
+  /**
+   * ## read
+   *
+   * Прочитать JWT токен, и получить uid пользователя.
+   *
+   * @param token
+   * */
+
+  fun read(token: String): String {
+    val claims: Claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).body
+    return claims.subject
+  }
+}
